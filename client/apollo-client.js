8939391a9abf8face56dd400da5caea729049b7c
Nicolás Otárola Teillier <nikolasotarola@gmail.com>
2023-04-25 04:04:36 -0400

    config amplify?

--- client/apollo-client.js
+++ client/apollo-client.js
@@ -1,4 +1,9 @@
 import { ApolloClient, InMemoryCache} from '@apollo/client';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+
 
 const client = new ApolloClient({
     uri: 'https://kgicpxvzm5.execute-api.us-east-1.amazonaws.com/dev' + '/graphql',
