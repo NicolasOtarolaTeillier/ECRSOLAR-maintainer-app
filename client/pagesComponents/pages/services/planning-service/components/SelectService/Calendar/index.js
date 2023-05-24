70db858a81c1764f20f7fb121b696b6c87dafeed
Nicolás Otárola Teillier <nikolasotarola@gmail.com>
2023-05-24 10:56:34 -0400

    test images

--- client/pagesComponents/pages/services/planning-service/components/SelectService/Calendar/index.js
+++ client/pagesComponents/pages/services/planning-service/components/SelectService/Calendar/index.js
@@ -5,6 +5,10 @@ import Grid from "@mui/material/Grid";
 
 // NextJS Material Dashboard 2 PRO components
 import MDBox from "/components/MDBox";
+import Image from "next/image";
+import IMAGEN1 from "../../../../../../../assets/images/IMAGEN1.jpg"; // Asegúrate de reemplazar 'my-image.jpg' con la ruta correcta de tu imagen.
+import IMAGEN2 from "../../../../../../../assets/images/IMAGEN2.jpg"; // Asegúrate de reemplazar 'my-image.jpg' con la ruta correcta de tu imagen.
+import IMAGEN3 from "../../../../../../../assets/images/IMAGEN3.jpg"; // Asegúrate de reemplazar 'my-image.jpg' con la ruta correcta de tu imagen.
 
 // NextJS Material Dashboard 2 PRO examples
 import DashboardLayout from "/examples/LayoutContainers/DashboardLayout";
@@ -17,10 +21,11 @@ import { useMaterialUIController, setSelectService } from "/context";
 // query
 import GET_ALL_SERVICES from "../../../../../../../api/service/queries.js";
 import { useQuery } from "@apollo/client";
+import { Box } from "@mui/material";
 
 function Calendar({ formData }) {
   const [open, setOpen] = useState(false);
-  const [idService,setIdService]= useState(0);
+  const [idService, setIdService] = useState(0);
   const { setActiveStep, activeStep } = formData;
   const [controller, dispatch] = useMaterialUIController();
   const { service } = controller;
@@ -51,14 +56,42 @@ function Calendar({ formData }) {
           default:
             name = "error";
         }
-        let a = ""
-        if (index === 5){
-          a = " Correctivo Electrico complejo"
-          name = "success"
+        let a = "";
+        if (index === 5) {
+          a = " Correctivo Electrico complejo";
+          name = "success";
+          return {
+            title: a,
+
+            // + ' - ' +service.photovoltaic_power_station.name.replace(
+            //   /(PFV|PMGD|PMG)/g,
+            //   ''
+            // ) +
+            // ' - ' +
+            // service.customer.fantasy_name,
+            end: diaSiguiente(service.finish_execution_date, 2),
+            start: service.proposed_execution_date,
+            id: service.id,
+            className: name,
+          };
         }
-        if (index === 6){
-          a = " Correctivo Electrico simple"
-          name = "error"
+        if (index === 6) {
+          a = " Correctivo Electrico simple";
+          name = "error";
+          return {
+            title: a,
+
+            // + ' - ' +service.photovoltaic_power_station.name.replace(
+            //   /(PFV|PMGD|PMG)/g,
+            //   ''
+            // ) +
+            // ' - ' +
+            // service.customer.fantasy_name,
+            end: diaSiguiente(service.finish_execution_date, 2),
+            start: service.proposed_execution_date,
+            id: service.id,
+            className: name,
+          };
         }
         return {
           title: service.service_type.name + a,
@@ -98,13 +131,15 @@ function Calendar({ formData }) {
 
                   if (value_className === "success") {
                     console.log("open modal");
-                    setOpen(true)
-                    setIdService(info.event.id)
+                    setOpen(true);
+                    setIdService(info.event.id);
                     return;
                   }
                   if (value_className != "error") {
+                    setOpen(false);
                     return;
                   }
+                  setOpen(false);
                   console.log(info.event.id);
                   const id = info.event.id;
                   setSelectService(dispatch, id);
@@ -115,26 +150,73 @@ function Calendar({ formData }) {
             // eslint-disable-next-line react-hooks/exhaustive-deps
             [dataService, open]
           )}
-          {open ? (()=>{
-            console.log(dataService)
-            const inicio = `Fecha : ${dataService?.filter(s=>s.id === idService)[0]?.start}`
-            const fin = ` - ${dataService?.filter(s=>s.id === idService)[0]?.end}`
-            return <>
-            <div>
-              {inicio} {fin}
-            </div>
-            <div>
-              Ejecucion: 
-            </div>
-            <div>
-              Produccion diaria: 
-            </div>
-            <div>
-              Hallasgos:
-            </div>
-            <div></div>
-            </>
-          })() : null}
+          {open
+            ? (() => {
+                console.log(dataService);
+                const inicio = `Fecha : ${
+                  dataService?.filter((s) => s.id === idService)[0]?.start
+                }`;
+                const fin = ` - ${
+                  dataService?.filter((s) => s.id === idService)[0]?.end
+                }`;
+                return (
+                  <>
+                    <Box>
+                      {inicio} {fin}
+                    </Box>
+                    <Box>Ejecucion:</Box>
+                    <Box>
+                      Producción diaria:
+                      <Box pl={2}>Lunes: 10 %</Box>
+                      <Box pl={2}>Martes: 30%</Box>
+                      <Box pl={2}>Miércoles: 55%</Box>
+                      <Box pl={2}>Jueves: 80%</Box>
+                      <Box pl={2}>Viernes: 100%</Box>
+                    </Box>
+                    <Box>
+                      Hallasgos:
+                      <Box>
+
+                      <Image
+                        src={IMAGEN1}
+                        alt="My Cool Image"
+                        width={500}
+                        height={300}
+                        />
+                      <Image
+                        src={IMAGEN2}
+                        alt="My Cool Image"
+                        width={500}
+                        height={300}
+                        />
+                      <Image
+                        src={IMAGEN3}
+                        alt="My Cool Image"
+                        width={500}
+                        height={300}
+                        />
+                        </Box>
+                    </Box>
+                    <Box>
+                      Proveedores:
+                      <Box pl={2}>
+                        Empresa:
+                        <Box pl={2}>
+                          Dartel:
+                          <Box pl={2}>
+                            - 2000 metros cable solar, negro 2000 metros
+                          </Box>
+                          <Box pl={2}>- cable solar rojo 500 pares mc4</Box>
+                          Monto neto:
+                          <Box pl={2}> $ 2.276.000 clp</Box>
+                        </Box>
+                      </Box>
+                    </Box>
+                    <div></div>
+                  </>
+                );
+              })()
+            : null}
         </Grid>
       </Grid>
     </MDBox>
